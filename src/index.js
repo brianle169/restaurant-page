23e5b4addd216fd4abe97bf3054cd94a94665cd3
Brian Le <83758821+brianle169@users.noreply.github.com>
2025-02-14 04:26:31 -0500

    Implemented About page. Added page selection feature with navigation buttons.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import "./style.css";
+import { AboutPage } from "./page_modules/about-page.js";
+import { MenuPage } from "./page_modules/menu-page.js";
+import { ReservationPage } from "./page_modules/reservation-page.js";
 
-console.log("Hello");
+const navBtns = document.querySelectorAll(".nav-btn");
+const content = document.getElementById("content");
+let currentPage = "";
+let currentTarget;
+
+let windowLoadHandler = () => {
+  content.appendChild(AboutPage());
+  navBtns[0].classList.add("selected");
+  currentPage = navBtns[0].value;
+  currentTarget = navBtns[0];
+};
+
+let navClickHandler = (event) => {
+  // remove selected class => for styling
+  currentTarget.classList.remove("selected");
+  // add classList to current target
+  event.target.classList.add("selected");
+  currentTarget = event.target;
+  switch (currentTarget.value) {
+    case "about": {
+      content.innerHTML = "";
+      content.appendChild(AboutPage());
+      break;
+    }
+    case "menu": {
+      content.innerHTML = "";
+      content.appendChild(MenuPage());
+      break;
+    }
+    case "reservation": {
+      content.innerHTML = "";
+      content.appendChild(ReservationPage());
+      break;
+    }
+  }
+};
+
+navBtns.forEach((button) => {
+  button.addEventListener("click", navClickHandler);
+});
+
+window.onload = windowLoadHandler;
---
+++ src/page_modules/menu-page.js
@@ -0,0 +1,48 @@
+import ab1 from "../../assets/ab1.jpg";
+import ab2 from "../../assets/ab2.jpg";
+
+export function MenuPage() {
+  // Create the div container for about page
+  const aboutPage = document.createElement("div");
+  aboutPage.classList.add("about");
+  aboutPage.id = "content";
+
+  // Start creating other elements
+  // 1. Title h1
+  const title = document.createElement("h1");
+  title.textContent = "Menu";
+
+  // 2. Description p
+  const desPara1 = document.createElement("p");
+  const desPara2 = document.createElement("p");
+  desPara1.classList.add("about-text");
+  desPara2.classList.add("about-text");
+  desPara1.textContent =
+    "Pétrus is a restaurant in London, England. It was opened in 2010 by chef Gordon Ramsay. Pétrus was awarded a Michelin star in 2011. The restaurant was named after the French wine Pétrus. Pétrus is located in Belgravia, London. The restaurant has a capacity of 60 seats. Pétrus is known for its French cuisine.";
+  desPara2.textContent =
+    "Pétrus offers a tasting menu and an à la carte menu. The tasting menu consists of 6 courses. The tasting menu costs £120. The à la carte menu consists of 3 courses. The à la carte menu costs £60. Pétrus offers a wine pairing with the tasting menu. The wine pairing costs £60.";
+
+  // 3. Image Gallery
+  const gallery = document.createElement("div");
+  gallery.classList.add("gallery");
+
+  // 4. Images
+  const img1 = document.createElement("img");
+  const img2 = document.createElement("img");
+  img1.src = ab1;
+  img1.alt = "ab1";
+  img2.src = ab2;
+  img2.alt = "ab2";
+
+  // Append elements to gallery
+  gallery.appendChild(img1);
+  gallery.appendChild(img2);
+
+  // Append elements to aboutPage
+  aboutPage.appendChild(title);
+  aboutPage.appendChild(desPara1);
+  aboutPage.appendChild(desPara2);
+  aboutPage.appendChild(gallery);
+
+  return aboutPage;
+}
